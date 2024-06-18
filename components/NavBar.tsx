1cce99c59deef8c23fec8b7106c119b90a67f1fd
Vidit Agrawal <viditagrawal56@gmail.com>
2024-06-18 23:29:10 +0530

    chore: changed some css

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,7 @@ import { Card, CardHeader, CardTitle, CardDescription } from "./ui/card";
 
 const NavBar = () => {
   return (
-    <Card className="bg-foreground rounded-none border-none flex items-center justify-between p-5">
+    <Card className="bg-primary rounded-none border-none flex items-center justify-between p-5">
       <h1 className="text-background font-bold text-lg">ExpenseEase</h1>
       <Avatar>
         <AvatarImage src="https://github.com/viditagrawal56.png" />
