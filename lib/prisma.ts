0a1c704c58d4af54efc3aabf1700fb4372202503
Vidit Agrawal <viditagrawal56@gmail.com>
2024-06-22 02:09:45 +0530

    feat: added google oauth

---
+++ lib/prisma.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const prismaClientSingleton = () => {
+  const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
+  const adapter = new PrismaNeon(neon);
+  return new PrismaClient({ adapter });
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
